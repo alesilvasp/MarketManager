b54ae72f5279b99b220adca6e73c82797e86e834
Alex Silva <alesilvasp@gmail.com>
2022-02-17 12:09:34 -0300

    fix: add app use ErrorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import { ErrorHandler, handleError } from "./errors/errorHandler";
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  (err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
+    handleError(err, res);
+  }
+);
 
 export default app;
