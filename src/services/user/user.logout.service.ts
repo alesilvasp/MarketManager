dee031a7edb4642cd33cce741747ae2ffd48e8f6
Eduardo Parraga <eduardo.parraga95@gmail.com>
2022-02-24 20:41:23 -0300

    work in progress

--- src/services/user/user.logout.service.ts
+++ src/services/user/user.logout.service.ts
@@ -1,25 +1,41 @@
 import { getRepository } from "typeorm";
 import { User, Cashier, Logs } from "../../entities";
 import AppError from "../../errors/appError";
-import * as bcrypt from "bcryptjs"
-import jwt, { JwtPayload } from "jsonwebtoken"
-import { config } from "../../config/jwt.config";
 import { IUserDecoded } from "../../interfaces/user/user.decoded.interface";
+
+function parseJwt (token: string) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
 
 export const userLogout = async (token: string) => {
 
     const userRepository = getRepository(User)
     const cashierRepository = getRepository(Cashier)
     const logsRepository = getRepository(Logs)
+    const tokenData: IUserDecoded = parseJwt(token)
+    
+    const user = await userRepository.findOne({ id: tokenData.id })
 
-    const decodedToken = jwt.decode(token, { complete: true })
+    if (!user) {
 
-    if (decodedToken) {
-
-
+        throw new AppError("User not found", 404)
     }
 
-    
-    
+    const cashiers = await cashierRepository.find()
+
+    const cashier = cashiers.find(item => item.user === user)
+
+    const logs = await logsRepository.find({
+        where: { 
+            logout: null,
+            user: user,
+        },
+    })
 
 }
